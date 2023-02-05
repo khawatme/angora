6f8a7218d1adf17df898dbd91e25cb67328b9d7e
Mohammed Khawatme <Mohammed.Khawatme@outlook.com>
2023-02-05 16:25:45 +0100

    angora

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slider-nav').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: false,
+  focusOnSelect: true,
+  centerMode: true,
+  prevArrow: false,
+  centerPadding: '0px',
+  nextArrow: false
+});
